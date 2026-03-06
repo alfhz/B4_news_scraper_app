615a5aa7531f6af4f688de5403401be4ef6483de
Nadhiefm <babandu1230@gmail.com>
2026-03-06 15:56:00 +0700

    feat(link_collector): tambah import library yang dibutuhkan

--- scraper/link_collector.py
+++ scraper/link_collector.py
@@ -1,18 +1,5 @@
-# TODO: implement get_article_links()
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from urllib.parse import urlparse, urljoin
 import time
-
-def get_article_links(url, limit):
-    # implementasi fungsi untuk mengambil semua link artikel dari halaman berita
-    # gunakan Selenium untuk mengambil link artikel
-    # jika halaman memiliki pagination, lanjutkan scraping ke halaman berikutnya
-    # hentikan proses jika jumlah link sudah mencapai limit
-    # kembalikan hasil dalam bentuk list berisi URL artikel
-    
-    # dummy data untuk testing
-    return [
-        "https://example.com/article1",
-        "https://example.com/article2",
-        "https://example.com/article3",
-    ][:limit]
